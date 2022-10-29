1c528edfd32707eff24794b0189b0fae61befea0
GoGiants1 <chooi221@snu.ac.kr>
2022-10-29 22:06:55 +0900

    Bump up to react-18

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -9,7 +9,6 @@ import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
 
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
 // Import root app
@@ -23,19 +22,21 @@ import reportWebVitals from 'reportWebVitals';
 
 // activate tailwindCSS
 import './index.css';
+// const MOUNT_NODE = document.getElementById('root') as HTMLElement;
+
+import { createRoot } from 'react-dom/client';
 
 const store = configureAppStore();
-const MOUNT_NODE = document.getElementById('root') as HTMLElement;
-
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container!); // createRoot(container!) if you use TypeScript
+root.render(
   <Provider store={store}>
     <HelmetProvider>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
+      {/* <React.StrictMode> */}
+      <App />
+      {/* </React.StrictMode> */}
     </HelmetProvider>
   </Provider>,
-  MOUNT_NODE,
 );
 
 // If you want to start measuring performance in your app, pass a function
