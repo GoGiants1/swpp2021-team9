eec974517c6148b0fc358c65be53045f49e0df1a
GoGiants1 <chooi221@snu.ac.kr>
2022-10-29 18:14:14 +0900

    Change to developement env

--- backend/app/band/apps.py
+++ backend/app/band/apps.py
@@ -3,12 +3,12 @@ from django.conf import settings
 
 
 class BandConfig(AppConfig):
-    name = 'band'
+    name = "band"
     first = True
 
     def ready(self):
         if self.first and settings.SCHEDULER_DEFAULT:
-            print("[cron] start")
-            from band import cron
-            cron.start()
+            # print("[cron] start")
+            # from band import cron
+            # cron.start()
             self.first = False
